9fd3be5f46c31412389012dc6b7cab85d8d7e313
Giorgi Samkharadze <gsamkharadze51@gmail.com>
2024-02-09 02:38:14 +0400

    add gitignore

---
+++ Rick and Morty/RickAndMortyApp.swift
@@ -0,0 +1,17 @@
+//
+//  Rick_and_MortyApp.swift
+//  Rick and Morty
+//
+//  Created by Giorgi Samkharadze on 09.02.24.
+//
+
+import SwiftUI
+
+@main
+struct RickAndMortyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
