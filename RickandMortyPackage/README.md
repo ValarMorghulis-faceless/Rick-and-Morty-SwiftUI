b3fed078641e52a4537810ba3700240888433dfc
Giorgi Samkharadze <gsamkharadze51@gmail.com>
2024-02-09 02:48:25 +0400

    changed patchof package

---
+++ RickandMortyPackage/README.md
@@ -0,0 +1,3 @@
+# RickandMortyPackage
+
+A description of this package.
---
+++ RickandMortyPackage/Sources/RickandMortyPackage/RickandMortyPackage.swift
@@ -0,0 +1,6 @@
+public struct RickandMortyPackage {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
